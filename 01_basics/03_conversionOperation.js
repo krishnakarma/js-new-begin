eb8d1c406a69a757f22b0dfe2c8d19e82dcfbaa5
krishnakarma <karmakrishna1704@gmail.com>
2024-07-09 13:42:56 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,4 +26,33 @@ let someNumber = 33
 
 let stringNumber  = String(someNumber)
 
-console.log(typeof stringNumber);
+console.log(typeof stringNumber)
+
+
+let  value = 3
+let negValue = -value
+console.log(2**2)
+console.log(negValue)
+
+let str1 = "krishna"
+let str2 = " karma"
+let str3 = str1+ str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1+"2")
+console.log(1+2+"2")
+console.log(+"");
+console.log(+true);
+
+let num1,num2,num3
+
+
+num1=num2=num3=4
+
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
