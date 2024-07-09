59031452034bff2bcfd71b129fb308b72f55fad2
krishnakarma <karmakrishna1704@gmail.com>
2024-07-09 12:10:09 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) we are using node js not browser
+
+//when to use space
+console.log(3 + 3); //  code readablity should be high
+console.log("krishna");
+
+let name = "krishnakarma";
+let age = 18;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+//bigintt forbig project such as reddit trading
+//string => ""
+//boolean => true / false
+//null =>standalone  value   null is a object
+//undefined
+//symbol =>unique
+//object
+
+console.log(typeof "krishnakarma");
+console.log(typeof undefined); //undefined bcz it is atype in js
+console.log(typeof null);// object
